9819718383557ddc9b1953fe9c66e7aa9db5bd10
Justin Snyder <jesst44@mail.rmu.edu>
2015-02-08 13:27:23 -0500

    0.1 PDT
    Proof of concept.

---
+++ PDT HOME/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	BindEventHandlers();
+});
+
+function BindEventHandlers() {
+	$('.nav-link').hover(function(){
+		var _this = $(this);
+		_this.addClass("nav-link-hover");
+	});
+
+	$('.star-detail').mouseenter(function(){
+		$(this).css({"color":"#666","margin-left":"5px"});
+
+	});
+
+	$('.star-detail').mouseleave(function(){
+		$(this).css({"color":"rgb(0, 43, 84)","margin-left":"0px"});
+
+	});
+
+	$('.star-detail').click(function(){
+		var _this = $(this);
+		$('.star-detail.underline').removeClass('underline');
+		_this.addClass('underline');
+	});
+
+	$('.pillar-block').hide();
+	$('.pillar-block').show("drop", 2000);
+}
+
